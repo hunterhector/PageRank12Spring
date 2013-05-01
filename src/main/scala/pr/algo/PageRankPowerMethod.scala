ff08b0ea3681e7f650456478520a789dce172b4e
hunterhector <hunterhector@gmail.com>
2013-04-30 22:31:46 -0400

    initial setup

---
+++ src/main/scala/pr/algo/PageRankPowerMethod.scala
@@ -0,0 +1,13 @@
+package pr.algo
+
+import breeze.linalg._
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Hector, Zhengzhong Liu
+ * Date: 4/30/13
+ * Time: 9:44 PM
+ */
+class PageRankPowerMethod {
+
+}
---
+++ src/main/scala/pr/model/TrecLikeResult.scala
@@ -0,0 +1,16 @@
+package PageRank12Spring.pr.model
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Hector
+ * Date: 2/21/13
+ * Time: 1:16 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class TrecLikeResult(val queryID: String, val docID: Int, val rank: Int, val score: Double, val runID: String) extends Result {
+  override def toString = ("%s\tQ0\t%s\t%s\t%.6f\t%s").format(queryID, docID, rank.toString, score, runID)
+}
+
+object TrecLikeResult {
+  val header = "QueryID\tQ0\tDocID\tRank\tScore\tRunID"
+}
